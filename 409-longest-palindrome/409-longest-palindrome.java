8791197146995fe635532a1c207cc9abc9d9258e
Joonha <dev.iinow.o@gmail.com>
2022-09-14 20:43:31 +0900

    Time: 4 ms (68.02%), Space: 41.9 MB (79.14%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,34 +1,19 @@
 class Solution {
     public int longestPalindrome(String s) {
-        var array = s.chars().toArray();
-        if (array.length == 1) {
-            return 1;
-        }
-
-        var map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < array.length; i++) {
-            if (!map.containsKey(array[i])) {
-                map.put(array[i], 0);
-            }
-            map.put(array[i], map.get(array[i]) + 1);
-        }
-
-        boolean hasRest = false;
-        int count = 0;
-        Iterator<Integer> iter = map.keySet().iterator();
-        while (iter.hasNext()) {
-            int key = iter.next();
-            int value = map.get(key);
-            if (value % 2 == 0) {
-                count += value;
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c);
             } else {
-                if (value / 2 != 0) {
-                    count += (value - 1);
-                }
-                hasRest = true;
+                set.add(c);
             }
         }
 
-        return hasRest ? count + 1 : count;
+        int remove = set.size();
+        if (remove > 0) {
+            remove -= 1;
+        }
+
+        return s.length() - remove;
     }
 }
