7e30ad58e722fa7e3f743beea28d597679ce9955
Joonha <dev.iinow.o@gmail.com>
2022-10-19 18:22:59 +0900

    Time: 39 ms (94.85%), Space: 43.8 MB (82.50%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,30 +3,23 @@ import java.util.*;
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
-        
-        // Sort - nlogn
         Arrays.sort(nums);
-        
-        // Two pointer closing with fixed element
+
         int closest = Integer.MAX_VALUE;
-        
-        for (int i = 0; i <= n - 3; i++) {
+
+        for (int i = 0; i < n - 2; i++) {
             int left = i + 1;
             int right = n - 1;
-            
+
             while (left < right) {
-                int current_triplet_sum = nums[i] + nums[left] + nums[right];
-                
-                // Update
-                if (Math.abs(target - closest) > Math.abs(target - current_triplet_sum)) {
-                    closest = current_triplet_sum;
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(target - closest) > Math.abs(sum - target)) {
+                    closest = sum;
                 }
-                
-                // Move pointers
-                if (current_triplet_sum < target) {
+
+                if (sum < target) {
                     left++;
-                }
-                else {
+                } else {
                     right--;
                 }
             }
