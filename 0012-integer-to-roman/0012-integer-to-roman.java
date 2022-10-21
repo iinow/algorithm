7bfec3fb054d9084236bc9b6b0eec21aebeb7414
Joonha <dev.iinow.o@gmail.com>
2022-10-21 09:02:14 +0900

    Time: 9 ms (71.63%), Space: 45.2 MB (68.68%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -2,58 +2,20 @@ import java.util.*;
 
 class Solution {
     public String intToRoman(int num) {
-        Map<Integer, String> map = Map.of(
-                1, "I",
-                5, "V",
-                10, "X",
-                50, "L",
-                100, "C",
-                500, "D",
-                1000, "M");
-        Map<String, String> temp = Map.of(
-                "V", "I",
-                "X", "I",
-                "L", "X",
-                "C", "X",
-                "D", "C",
-                "M", "C");
-        List<Integer> values = new ArrayList<>();
-        map.forEach((k, v) -> {
-            values.add(k);
-        });
-        values.sort((a, b) -> a - b);
+        int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-        int gisu = 0;
-        String s = "";
-        while (num != 0) {
-            int rest = num % 10;
-            int mok = num / 10;
-            if (rest == 4 || rest == 9) {
-                String v = map.get((rest + 1) * (int) Math.pow(10, gisu));
-                s = temp.get(v) + v + s;
-            } else {
-                // 1, 2, 3, 5, 6, 7, 8, 0
-                if (rest != 0) {
-                    int v = rest * (int) Math.pow(10, gisu);
+        StringBuilder romanNumeral = new StringBuilder();
 
-                    String left = "";
-                    for (int i = values.size() - 1; i >= 0; i--) {
-                        int mok2 = v / values.get(i);
-                        if (mok2 == 0) {
-                            continue;
-                        }
-
-                        for (int j = 0; j < mok2; j++) {
-                            left = left + map.get(values.get(i));
-                        }
-                        v = v % values.get(i);
-                    }
-                    s = left + s;
-                }
+        for (int i = 0; i < value.length && num > 0; i++) {
+            // Check from the highest value, 1000 to the smallest.
+            // If it is possible to subtract, for example 400, we know the roman numeral
+            // next is "CD".
+            while (num >= value[i]) {
+                romanNumeral.append(roman[i]);
+                num -= value[i];
             }
-            num = mok;
-            gisu++;
         }
-        return s;
+        return romanNumeral.toString();
     }
 }
