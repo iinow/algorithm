8b8c915cc4c3d5c17334055c26f5421cd772a608
Joonha <dev.iinow.o@gmail.com>
2022-10-25 21:03:31 +0900

    Time: 15 ms (63.08%), Space: 42.5 MB (80.12%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            boolean res = s.indexOf(s.substring(i, i + 1)) == t.indexOf(t.substring(i, i + 1));
+            if (res) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
