cb0a1e81fdc02a7e0c0f42b8a9c71cdb8c2cac0f
Joonha <dev.iinow.o@gmail.com>
2022-11-03 23:03:26 +0900

    Time: 2 ms (85.56%), Space: 50.2 MB (67.23%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,21 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Boolean> map = new HashMap<>();
+        int sum = 0;
         for (int num: nums) {
-            if (map.get(num) == null) {
-                map.put(num, false);
-                continue;
-            }
-            map.put(num, true);
+            sum ^= num;
         }
-        
-        Iterator<Integer> iter = map.keySet().iterator();
-        while(iter.hasNext()) {
-            int key = iter.next();
-            if (!map.get(key)) {
-                return key;
-            }
-        }
-        return 1;
+        return sum;
     }
 }
