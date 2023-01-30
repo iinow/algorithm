24ef2933c632ea77a2d0f9db9aa6d811975949bf
Joonha <dev.iinow.o@gmail.com>
2023-01-30 22:41:35 +0900

    Time: 1 ms (100.00%), Space: 39.3 MB (94.21%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 0) return false;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+                continue;
+            }
+            if (n % 3 == 0) {
+                n /= 3;
+                continue;
+            }
+            if (n % 5 == 0) {
+                n /= 5;
+                continue;
+            }
+            return false;
+        }
+        
+        return true;
+    }
+}
